9980e76c59b24e74a9947b79188d43287ebf8562
FREDYK1 <fkwekukankam@gmail.com>
2025-01-17 22:12:36 +0000

    Testing module

--- invoice.py
+++ invoice.py
@@ -5,7 +5,7 @@ from fpdf import FPDF
 from pathlib import Path
 
 
-def generate(invoices_path, pdfs_path, product_id, product_name, amount_purchased, price_per_unit, total_price):
+def generate(invoices_path, pdfs_path, image, product_id, product_name, amount_purchased, price_per_unit, total_price):
     filepaths = glob.glob(f"{invoices_path}/*.xlsx")
     """
     Generates PDF invoices from Excel files.
@@ -37,10 +37,10 @@ def generate(invoices_path, pdfs_path, product_id, product_name, amount_purchase
         invoice_nr, date = filename.split("-")
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, text=f"Invoice nr.{invoice_nr}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
         pdf.set_font(family="Times", size=16, style="B")
-        pdf.cell(w=50, h=8, text=f"Date: {date}", ln=1)
+        pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
         df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
@@ -49,39 +49,41 @@ def generate(invoices_path, pdfs_path, product_id, product_name, amount_purchase
         columns = [item.replace("_", " ").title() for item in columns]
         pdf.set_font(family="Times", size=10, style="B")
         pdf.set_text_color(80, 80, 80)
-        pdf.cell(w=30, h=8, text=columns[0], border=1)
-        pdf.cell(w=70, h=8, text=columns[1], border=1)
-        pdf.cell(w=30, h=8, text=columns[2], border=1)
-        pdf.cell(w=30, h=8, text=columns[3], border=1)
-        pdf.cell(w=30, h=8, text=columns[4], border=1, ln=1)
+        pdf.cell(w=30, h=8, txt=columns[0], border=1)
+        pdf.cell(w=70, h=8, txt=columns[1], border=1)
+        pdf.cell(w=30, h=8, txt=columns[2], border=1)
+        pdf.cell(w=30, h=8, txt=columns[3], border=1)
+        pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
         # Add rows to the table
         for index, row in df.iterrows():
             pdf.set_font(family="Times", size=10)
             pdf.set_text_color(80, 80, 80)
-            pdf.cell(w=30, h=8, text=str(row[product_id]), border=1)
-            pdf.cell(w=70, h=8, text=str(row[product_name]), border=1)
-            pdf.cell(w=30, h=8, text=str(row[amount_purchased]), border=1)
-            pdf.cell(w=30, h=8, text=str(row[price_per_unit]), border=1)
-            pdf.cell(w=30, h=8, text=str(row[total_price]), border=1, ln=1)
+            pdf.cell(w=30, h=8, txt=str(row[product_id]), border=1)
+            pdf.cell(w=70, h=8, txt=str(row[product_name]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[amount_purchased]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[price_per_unit]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[total_price]), border=1, ln=1)
 
         total_sum = df[total_price].sum()
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
-        pdf.cell(w=30, h=8, text="", border=1)
-        pdf.cell(w=70, h=8, text="", border=1)
-        pdf.cell(w=30, h=8, text="", border=1)
-        pdf.cell(w=30, h=8, text="", border=1)
-        pdf.cell(w=30, h=8, text=str(total_sum), border=1, ln=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=70, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
         # Add total sum sentence
         pdf.set_font(family="Times", size=10, style="B")
-        pdf.cell(w=30, h=8, text=f"The total price is {total_sum}", ln=1)
+        pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
 
         # Add company name and logo
         pdf.set_font(family="Times", size=14, style="B")
-        pdf.cell(w=25, h=8, text=f"PythonHow")
-        pdf.image("pythonhow.png", w=10)
+        pdf.cell(w=25, h=8, txt=f"PythonHow")
+        pdf.image(image, w=10)
 
-        os.makedirs(pdfs_path, exist_ok=True)
+        if not os.path.exists(pdfs_path):
+            os.makedirs(pdfs_path, exist_ok=True)
+
         pdf.output(f"{pdfs_path}/{filename}.pdf")
